228811d951e8504e7669db339a32d154f2922f3d
bubeez <christopher.blount@outlook.com>
2020-08-30 00:09:32 -0500

    widget created

---
+++ js/index.js
@@ -0,0 +1,26 @@
+function discordAPI(){
+    let init = {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'reload'
+    }
+
+    fetch('https://discordapp.com/api/guilds/411607476430176256/widget.json', init).then(function(response){
+        if(response.status != 200){
+          console.log("it didn't work " + response.status);
+          return
+        }
+        response.json().then(function(data){
+        let serverName = data.name;
+        let onlineCount = data.presence_count;
+        let output;
+        if (onlineCount == 1){
+            output = `${serverName} | ${onlineCount} Member Online`;
+        } else {
+            output = `${serverName} | ${onlineCount} Members Online`;
+        }
+        document.getElementById('discord-text').innerText = output;
+        })
+    })
+}
+discordAPI()
