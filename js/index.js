33fbf38a40b49f502114355b609ddca8aedcb03f
bubeez <christopher.blount@outlook.com>
2020-09-01 01:03:24 -0500

    Lots of updates

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function unhover(element) {
+    element.setAttribute('src', '../images/discord_sm_blurple.png');
+  }
+  
+  function hover(element) {
+    element.setAttribute('src', '../images/discord_sm.png');
+  }
