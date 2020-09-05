baf2e5a340d51a9c31d272b688737d9f01956a34
bubeez <christopher.blount@outlook.com>
2020-09-05 08:58:13 -0500

    big update

--- _site/js/index.js
+++
@@ -1,7 +0,0 @@
-function unhover(element) {
-    element.setAttribute('src', 'https://bubeez.github.io/bthd/images/discord_sm_blurple.png');
-  }
-  
-  function hover(element) {
-    element.setAttribute('src', 'https://bubeez.github.io/bthd/images/discord_sm.png');
-  }
